10ce5702363966e38954e59c334d8fdec21429a2
AyaElsawyElghaysh <ayah.elghaysh.12345@gmail.com>
2021-06-18 17:55:59 +0200

    upload files

---
+++ bo/New-master/app/src/main/java/com/example/myproject/character/framgentfoeachchar.java
@@ -0,0 +1,32 @@
+package com.example.myproject.character;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.myproject.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class framgentfoeachchar extends Fragment {
+
+
+    public framgentfoeachchar() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_framgentfoeachchar, container, false);
+    }
+
+}
